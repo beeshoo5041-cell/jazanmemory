b2f5a2b780ebeb98dfd69becf1d87cbe814741bc
beeshoo5041-cell <beeshoo.5041@gmail.com>
2026-06-21 22:15:36 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,2 @@
-export const firebaseConfig = {
-  apiKey: "PUT_YOUR_API_KEY_HERE",
-  authDomain: "PUT_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PUT_YOUR_PROJECT_ID",
-  storageBucket: "PUT_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PUT_SENDER_ID",
-  appId: "PUT_APP_ID"
-};
-
-export const adminEmails = [
-  "beeshoo.5041@gmail.com"
-];
+export const firebaseConfig = { apiKey: "PUT_YOUR_API_KEY_HERE", authDomain: "PUT_YOUR_PROJECT.firebaseapp.com", projectId: "PUT_YOUR_PROJECT_ID", storageBucket: "PUT_YOUR_PROJECT.appspot.com", messagingSenderId: "PUT_SENDER_ID", appId: "PUT_APP_ID"};
+export const adminEmails = ["beeshoo.5041@gmail.com"];
